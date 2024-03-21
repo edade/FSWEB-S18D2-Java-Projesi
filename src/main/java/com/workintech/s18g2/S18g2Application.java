cf98b52f68619b2499b6554cdce2182aa95b262f
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-03-21 15:35:20 +0300

    dependency

---
+++ src/main/java/com/workintech/s18g2/S18g2Application.java
@@ -0,0 +1,13 @@
+package com.workintech.s18g2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S18g2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S18g2Application.class, args);
+	}
+
+}
